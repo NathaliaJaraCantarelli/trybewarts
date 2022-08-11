5c35af9d76448c467e35c5211426027e554a3b2d
Joana <joanamdsantos1@gmail.com>
2022-08-11 18:13:20 -0300

    tentando solucionar a validação do login

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 window.onload = function () {
-    let button = document.getElementById('submit-button');
+    let button = document.getElementById('submit-button')
     let inputEmail = document.getElementById('input-email').value;
     let inputPassword = document.getElementById ('input-password').value;
+    let validEmail = 'tryber@teste.com'; 
+    let validPassword = '123456';
 
 
-    function validateLogin () {
-        if (inputEmail === "tryber@teste.com" && inputPassword === "123456") {
-            alert('Olá, Tryber!');
-        }
+    function validateLogin() {
+        if (inputEmail == validEmail && inputPassword == validPassword) {
+            alert("Olá, Tryber!");
+        } else {
             alert("Email ou senha inválidos");
         }
+    }
 
         button.addEventListener('click', validateLogin);
     }
-
